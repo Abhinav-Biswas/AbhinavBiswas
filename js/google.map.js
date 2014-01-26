c217a877cda2e51cebe35ca21b807fa9305560f4
Abhinav Biswas <abhinav.the.prince.7star@gmail.com>
2014-01-26 22:54:57 +0530

    Major Update

--- js/google.map.js
+++ js/google.map.js
@@ -1,14 +1,23 @@
 //// Start Google Maps ////
 function initialize() {
   var myLatlng = new google.maps.LatLng(22.768072,88.398283);
+  var myLatlng2 = new google.maps.LatLng(17.469111,78.57688);
   var myOptions = {
 	zoom: 12,
 	center: myLatlng,
 	mapTypeId: google.maps.MapTypeId.TERRAIN
   }
+  var myOptions2 = {
+	zoom: 14,
+	center: myLatlng2,
+	mapTypeId: google.maps.MapTypeId.TERRAIN
+  }
   var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  var map2 = new google.maps.Map(document.getElementById("map_canvas2"), myOptions2);
   var marker = new google.maps.Marker({position: myLatlng}); 
   marker.setMap(map);
+  var marker2 = new google.maps.Marker({position: myLatlng2, title:"ECIL, Hyderabad", map: map2, labelContent: "ECIL Hyderabad"}); 
+  marker2.setMap(map2);
 }
 
 function loadScript() {
